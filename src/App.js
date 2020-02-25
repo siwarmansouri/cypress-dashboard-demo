0c7a7b96250db8f65f99b0e5f92e6c7fafc89832
mansourisiwar <siwar.mansouri@vistaprint.com>
2020-02-25 11:26:37 +0100

    DG-30528: learning react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const key = 'title';
+const list = [
+  {
+    [key]: 'React',
+    url: 'https://reactjs.org',
+    target: '_blank',
+    objectID: 0
+  },
+  {
+    [key]: 'Redux',
+    url: 'https://redux.js.org',
+    target: '_blank',
+    objectID: 1
+  }
+]
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }  
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList});
+  }
+  restoreAll() {
+    this.setState({list});
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p> 
+          {this.state.list.map( item => 
+            <div key={item.objectID}> 
+            <span><a className="App-link" href={item.url} target={item.target} rel="noopener noreferrer">
+            Learn {item.title}
+            </a> 
+            </span>
+            <span>
+              <button onClick={()=> this.onDismiss(item.objectID)}
+              type="button">Dismiss</button>
+            </span>
+            </div>
+          )} 
+              <button onClick={()=> this.restoreAll()}
+              type="button">Restore All</button>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
